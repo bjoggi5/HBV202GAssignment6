7fc709c24dac7cedcda62cb94fc9c340f158c5cb
bjoggi5 <baj24@hi.is>
2023-02-28 12:24:17 +0000

    Assignment 6

--- src/test/java/is/hi/hbv202g/ass6/CarOwnerTest.java
+++ src/test/java/is/hi/hbv202g/ass6/CarOwnerTest.java
@@ -1,16 +1,28 @@
 package is.hi.hbv202g.ass6;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CarOwnerTest {
 
+    private static final String BEETLE = "Beetle";
+    private static final String JOHN_DOE = "John Doe";
+    private Car car;
+    private CarOwner carOwner;
+
+
+    @Before
+    public void setUp() throws Exception {
+        car = new Car(BEETLE);
+        carOwner = new CarOwner(JOHN_DOE, car);
+    }
+
     @Test
     public void getName() {
-        Car car=new Car("Beetle");
-        CarOwner carOwner = new CarOwner("John Doe", car);
-        assertEquals("John Doe", carOwner.getName());
+        assertEquals(JOHN_DOE, carOwner.getName());
     }
+
 
 }
--- src/test/java/is/hi/hbv202g/ass6/CarTest.java
+++ src/test/java/is/hi/hbv202g/ass6/CarTest.java
@@ -1,15 +1,22 @@
 package is.hi.hbv202g.ass6;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CarTest {
-    @Test
-    public void getNameOfCar() {
-        Car car=new Car("Beetle");
-        CarOwner carOwner = new CarOwner("John Doe", car);
-        assertEquals("Beetle", carOwner.getNameOfCar());
+
+    private static final String BEETLE = "Beetle";
+    private Car car;
+
+    @Before
+    public void setUp() throws Exception {
+        car = new Car(BEETLE);
     }
 
+    @Test
+    public void getNameOfCar() {
+        assertEquals(BEETLE, car.getName());
+    }
 }
